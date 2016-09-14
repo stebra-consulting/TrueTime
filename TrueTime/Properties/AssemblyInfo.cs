161181378da6085081a7c919c3ce8032a83ed4ab
Thomas J. Ekman <thomas.ekman@stebra.se>
2016-09-14 17:03:12 +0200

    added some information

--- TrueTime/Properties/AssemblyInfo.cs
+++ TrueTime/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TrueTime")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A time reporting program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TrueTime")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Stebra Consulting AB, 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
